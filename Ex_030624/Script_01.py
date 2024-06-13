e01f27d90207d1f60a4e9297948583976c25f2fc
meenal <meenalsaloni82@gmail.com>
2024-06-13 06:55:24 +0530

    p3xlearning

---
+++ Ex_030624/Script_01.py
@@ -0,0 +1,8 @@
+# This is single line comment
+print("love Avyukt")
+print("love Avyukt")
+print("love Avyukt")  # use ctrl D for duplicate the line
+print(2 * 2)
+print(2 + 2)
+print(2 - 2)
+print(2 / 2)
