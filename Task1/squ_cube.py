df68bbe369d24718d81381653bc7133fc11147b4
meenal <meenalsaloni82@gmail.com>
2024-06-13 06:55:01 +0530

    p3xlearning

---
+++ Task1/squ_cube.py
@@ -0,0 +1,7 @@
+# Develop a Python script that calculates the square and cube of a given number.
+# num = 2 sq - 4, c = 8
+#num = int(input("Enter number :"))
+num=2
+suqare = num**2
+cube = num**3
+print(suqare, cube)
